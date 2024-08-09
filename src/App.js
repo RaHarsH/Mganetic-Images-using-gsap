c598f51ade10b0f157f46a7ce0cb66cd24aec72a
Harshith R <harshith.raju0710@gmail.com>
2024-08-09 14:21:31 +0530

    added more images with magnetic effect

--- src/App.js
+++ src/App.js
@@ -5,10 +5,16 @@ import img1 from './images/1.jpg';
 import img2 from './images/2.jpg';
 import img3 from './images/3.jpg';
 import img4 from './images/4.jpg';
+import img5 from './images/5.jpg';
+import img6 from './images/6.jpg';
+import img7 from './images/7.jpg';
+import img8 from './images/8.jpg';
+import img9 from './images/9.jpg';
+import img10 from './images/10.jpg';
 import gsap from 'gsap';
 import { ScrollTrigger } from 'gsap/all';
 
-const images = [img1, img2, img3, img4];
+const images = [img1, img2, img3, img4, img5, img6, img7, img8, img9, img10];
 
 function App() {
   gsap.registerPlugin(ScrollTrigger)
@@ -26,14 +32,14 @@ function App() {
           y: '0%',
           opacity: 1,
           ease: 'power2.out',
-          duration: 1,
+          duration: 1.25,
           stagger: 1,
           // scrollTrigger: {
           //   trigger: imageRef,
           //   start: 'top 40%',
           //   end: 'top 30%',
           //   scrub: 1,
-          //   toggleActions: 'play none none repeat'
+          //   // toggleActions: 'play none none repeat'
           // }
         }
       );
@@ -42,7 +48,7 @@ function App() {
 
   return (
     <>
-      <h1>Interact with images.</h1>
+      <h1>Interact with the images.</h1>
       <div className="App">
         <div className="grid-container">
           {images.map((image, index) => (
